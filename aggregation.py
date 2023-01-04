eef9d2ce4d0fb3de262df8d43891a2105887a380
chaozhang <1244382307@qq.com>
2023-01-04 12:20:29 +0000

    fedadam

--- aggregation.py
+++ aggregation.py
@@ -1,5 +1,6 @@
 import wandb
 import copy
+import torch
 
 class My_ClassificationAggregator():
 	def get_model_params(self):
@@ -11,23 +12,34 @@ class My_ClassificationAggregator():
 		self.model.load_state_dict(model_parameters)
 
 	def torch_aggregator(self, raw_grad_list, training_num):
+		if not hasattr(self,'optimizer'):
+			self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
 		if hasattr(self,'aggregation_num'):
 			self.aggregation_num+=1
 		else:
 			self.aggregation_num=1
 		print(f'round:{self.aggregation_num}')
-		(num0, avg_params) = raw_grad_list[0]# 取出server端
-		for k in avg_params.keys():      # 遍历server端的每一个参数
-			for i in range(0, len(raw_grad_list)):   # 当server端参数固定后，遍历其它client端与server对应的参数
+
+		tmp_param = copy.deepcopy(self.model.state_dict())
+		self.optimizer.zero_grad()
+
+		val_score = 0
+
+		for k,param in self.model.named_parameters():
+			for i in range(0, len(raw_grad_list)):
 				local_sample_number, local_model_params = raw_grad_list[i]
 				w = local_sample_number / training_num
-				
-				# local_sample_number是挑选的当前的client训练样本数，training_num是所有client训练的样本数
-				if i == 0:
-					avg_params[k] = local_model_params[k] * w
-				
+				if k=='val_score':
+					val_score += local_model_params[k]*w
+				if param.grad is None:
+					param.grad = (tmp_param[k]-local_model_params[k]) * w
 				else:
-					avg_params[k] += local_model_params[k] * w
+					param.grad += (tmp_param[k]-local_model_params[k]) * w
+		
+		self.optimizer.step()
+		avg_params = self.model.state_dict()
+		avg_params['val_score'] = val_score
+
 		if hasattr(self,'val_score') and avg_params['val_score']>self.val_score:
 			self.val_score=avg_params['val_score']
 			self.best_model = avg_params
